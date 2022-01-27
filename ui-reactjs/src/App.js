c231a43e75975cc8f5e890e9e1a0d9e78f684c49
Pritam Prasad <pritam.prasd@gmail.com>
2022-01-27 21:09:09 +0530

    colors

--- ui-reactjs/src/App.js
+++ ui-reactjs/src/App.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import {BASE_URL, call_get_api, call_post_api} from './api'
 
 function App() {
   const [response, setResponse] = useState({})
   const [data, setData] = useState({})
   const [msg, setMsg] = useState('')
-  const [age, setAge] = useState('1')
-  const BASE_URL='http://192.168.1.7:5000'
+  const [age, setAge] = useState('30')
 
   useEffect(() => {
     console.log('getting pubkey...')
@@ -16,44 +16,17 @@ function App() {
       .catch(err => console.error(err))
   }, [])
 
-  function encrypt(request) {
-    return JSON.stringify(request)
-  }
-
-  function call_post_api() {
-    fetch(BASE_URL + '/data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: encrypt({
-        'age': Number(age),
-        'message': msg
-      })
-    })
-      .then(response => response.json())
-      .then(res => setData(res))
-      .catch(err => console.error(err))
-  }
-
-  function call_api() {
-    fetch(BASE_URL + '/hello')
-      .then(response => response.json())
-      .then(res => setResponse(res['message']))
-      .catch(err => console.log(err))
-  }
-
   return (
     <div className="App">
-      <button onClick={call_api}>GET /hello</button>
+      <button onClick={() => call_get_api(setResponse)}>GET /hello</button>
       <div className='api_response'>
       <div className="text-header">API Response:</div> {JSON.stringify(response)}
       </div>
         msg: 
       <input type="text" onChange={(e) => setMsg(e.target.value)} />
       Age:{age}
-      <input type="range" min='1' max='100' step='1'  onChange={(e) => setAge(e.target.value)}/>
-      <button onClick={call_post_api}>POST /data</button>
+      <input type="range" min='1' max='100' step='1' value={age} onChange={(e) => setAge(e.target.value)}/>
+      <button onClick={() => call_post_api(age, msg, setData)}>POST /data</button>
       <div className='api_response'>
         <div className="text-header">API Response:</div> {JSON.stringify(data)}
       </div>
