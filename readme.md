fa466d04f1b775e5eb660b284e9b22f192920783
Pritam Prasad <pritam.prasd@gmail.com>
2022-01-28 02:38:18 +0530

    intermediate transfer

--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 - Create private key: `openssl ecparam -name prime256v1 -genkey -noout -out private.ec.key`
 - Create public key: `openssl ec -in private.ec.key -pubout -out public.pem`
 - Encrypt: `openssl cms -encrypt -binary -aes-256-cbc -in plaintext.dat -out ciphertext.dat p256-cert.pem`
+
+
+- wasm build: `wasm-pack build`
+- FE start: `yarn start`
--- ui-reactjs/src/App.js
+++ ui-reactjs/src/App.js
@@ -1,8 +1,27 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import {BASE_URL, call_get_api, call_post_api} from './api'
+import * as wasm from 'mycrypto'
+// import wasm from './crypto_bg'
 
 function App() {
+  // const [wasm, setWasm] = useState({})
+
+
+  // useEffect(() => { 
+  //   const fetchWasm = async () => {
+  //     try {
+  //       console.log('Trying to load')
+  //       const wasm = await fetch('crypto_bg.wasm');
+  //       setWasm(wasm)
+  //       console.log('Loaded!!!')
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   };
+  //   fetchWasm();
+  // }, []);
+
   const [response, setResponse] = useState({})
   const [data, setData] = useState({})
   const [msg, setMsg] = useState('')
@@ -16,8 +35,10 @@ function App() {
       .catch(err => console.error(err))
   }, [])
 
+
   return (
     <div className="App">
+      <div>Greet: {wasm && wasm.greet('pp')}</div>
       <button onClick={() => call_get_api(setResponse)}>GET /hello</button>
       <div className='api_response'>
       <div className="text-header">API Response:</div> {JSON.stringify(response)}
